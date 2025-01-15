f708f963141f55269177eb2beff7afa84c38bded
hoos <hoos@tf.uni-freiburg.de>
2025-01-15 18:05:21 +0100

    Fix path

--- gift_eval/evaluate.py
+++ gift_eval/evaluate.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 # Add parent directory to Python path
-sys.path.append(str(Path(__file__).parent))
+sys.path.append(str(Path(__file__).parent.parent))
 
 
 import csv
