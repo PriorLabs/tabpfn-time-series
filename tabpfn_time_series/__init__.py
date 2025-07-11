93295c645eeaccf3135ab0a2d0283e9fbbb415bf
Brandon Williams <bwilliams9@pm.me>
2025-07-11 11:46:01 +0200

    Pull Python package version from pyproject.toml
    So that the Python package and uv project versions stay in sync.
    
    Right now, a `pip install .` would result in a package version of
    0.1.0, even though the package should be on 1.0.0.

--- tabpfn_time_series/__init__.py
+++ tabpfn_time_series/__init__.py
@@ -1,8 +1,21 @@
+from pathlib import Path
+
+try:
+    # For Python 3.11+
+    import tomllib
+
+    # For Python <3.11
+    # import tomli as tomllib
+except ImportError:
+    import tomli as tomllib
+
+with (Path(__file__).parent.parent / "pyproject.toml").open("rb") as f:
+    __version__ = tomllib.load(f)["project"]["version"]
+
+
 from .features import FeatureTransformer
 from .predictor import TabPFNTimeSeriesPredictor, TabPFNMode
 from .defaults import TABPFN_TS_DEFAULT_QUANTILE_CONFIG
-
-__version__ = "0.1.0"
 
 __all__ = [
     "FeatureTransformer",
